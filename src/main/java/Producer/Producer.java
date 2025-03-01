8f5bee27df9247dd3825cb1dc36baa1ba220898e
Jiahe Jiang <61075605+Isrothy@users.noreply.github.com>
2025-02-28 21:02:47 -0600

    refactor

--- src/main/java/Producer/Producer.java
+++ src/main/java/Producer/Producer.java
@@ -2,6 +2,8 @@ package Producer;
 
 import MarketDataQueryType.MarketDataQueryType;
 import Producer.QueryGenerator.QueryGenerator;
+import DataChannel.DataChannel;
+import DataChannel.ChannelException;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -11,59 +13,17 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
 
-class NovaPipeWebSocket implements WebSocket.Listener {
-
-    @Override
-    public void onOpen(WebSocket webSocket) {
-        System.out.println("WebSocket opened.");
-        webSocket.request(1);
-        WebSocket.Listener.super.onOpen(webSocket);
-    }
-
-    @Override
-    public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
-        System.out.println("Received message: " + data);
-        webSocket.request(1);
-        return CompletableFuture.completedFuture(null);
-    }
-
-    @Override
-    public CompletionStage<?> onPing(WebSocket webSocket, ByteBuffer message) {
-        System.out.println("Received ping: " + new String(message.array()));
-        webSocket.sendPong(message);
-        webSocket.request(1);
-        return CompletableFuture.completedFuture(null);
-    }
-
-    @Override
-    public CompletionStage<?> onPong(WebSocket webSocket, ByteBuffer message) {
-        System.out.println("Received pong: " + new String(message.array()));
-        webSocket.request(1);
-        return CompletableFuture.completedFuture(null);
-    }
-
-    @Override
-    public CompletionStage<?> onClose(WebSocket webSocket, int statusCode, String reason) {
-        System.out.println("WebSocket closed: " + statusCode + " " + reason);
-        return CompletableFuture.completedFuture(null);
-    }
-
-    @Override
-    public void onError(WebSocket webSocket, Throwable error) {
-        System.out.println("WebSocket error: " + error.getMessage());
-    }
-}
-
 public class Producer {
     private final QueryGenerator gen;
     private final String product;
     private final MarketDataQueryType type;
+    private final DataChannel channel;
 
-
-    public Producer(QueryGenerator gen, String product, MarketDataQueryType type) {
+    public Producer(QueryGenerator gen, String product, MarketDataQueryType type, DataChannel channel) {
         this.product = product;
         this.type = type;
         this.gen = gen;
+        this.channel = channel;
     }
 
     public void produceRawExchangeData() throws Exception {
@@ -80,4 +40,60 @@ public class Producer {
 
         Thread.currentThread().join();
     }
+
+
+    class NovaPipeWebSocket implements WebSocket.Listener {
+
+        @Override
+        public void onOpen(WebSocket webSocket) {
+            System.out.println("WebSocket opened.");
+            webSocket.request(1);
+            WebSocket.Listener.super.onOpen(webSocket);
+        }
+
+        @Override
+        public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
+            System.out.println("Received message: " + data);
+            try {
+                channel.send(String.format("""
+                                    {
+                                        "tag": "%s@%s",
+                                        "payload": %s
+                                    }
+                                """
+                        , gen.getTag(), type, data.toString()));
+            } catch (ChannelException e) {
+                throw new RuntimeException(e);
+            }
+            webSocket.request(1);
+            return CompletableFuture.completedFuture(null);
+        }
+
+        @Override
+        public CompletionStage<?> onPing(WebSocket webSocket, ByteBuffer message) {
+            System.out.println("Received ping: " + new String(message.array()));
+            webSocket.sendPong(message);
+            webSocket.request(1);
+            return CompletableFuture.completedFuture(null);
+        }
+
+        @Override
+        public CompletionStage<?> onPong(WebSocket webSocket, ByteBuffer message) {
+            System.out.println("Received pong: " + new String(message.array()));
+            webSocket.request(1);
+            return CompletableFuture.completedFuture(null);
+        }
+
+        @Override
+        public CompletionStage<?> onClose(WebSocket webSocket, int statusCode, String reason) {
+            System.out.println("WebSocket closed: " + statusCode + " " + reason);
+            return CompletableFuture.completedFuture(null);
+        }
+
+        @Override
+        public void onError(WebSocket webSocket, Throwable error) {
+            System.out.println("WebSocket error: " + error.getMessage());
+        }
+    }
+
 }
